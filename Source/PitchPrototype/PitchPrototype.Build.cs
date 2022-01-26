547fb86c70b2d786c4305a648653f6eb01ff2dce
mrzebrasmoothie <averyweibel@yahoo.com>
2022-01-26 00:43:37 -0800

    Build configuration

--- Source/PitchPrototype/PitchPrototype.Build.cs
+++ Source/PitchPrototype/PitchPrototype.Build.cs
@@ -7,6 +7,8 @@ public class PitchPrototype : ModuleRules
 	public PitchPrototype(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+		
+		bEnableExceptions = true;
 
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "AIModule", "GameplayTasks" });
 	}
