847209c98db9cdae9764ddc3d05982b80f5e578b
Ly Phung <lynphung@ucsc.edu>
2022-03-31 11:02:01 -0700

    player reply

---
+++ Source/PitchPrototype/PlayerDialogueChoice.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Components\TextBlock.h"
+#include "PlayerDialogueChoice.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PITCHPROTOTYPE_API UPlayerDialogueChoice : public UUserWidget
+{
+	GENERATED_BODY()
+};
