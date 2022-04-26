516771b57190025c4c035b2fe027a016001aa106
Ly Phung <lynphung@ucsc.edu>
2022-04-26 01:09:02 -0700

    ai improvements

--- Source/PitchPrototype/StateMC_LockedOnSwordSwing.cpp
+++ Source/PitchPrototype/StateMC_LockedOnSwordSwing.cpp
@@ -32,7 +32,7 @@ void StateMC_LockedOnSwordSwing::Execute(float DeltaTime)
 	auto hitAI = Cast<ABaseAICharacter>(mainCharacter->weapon->overlappedPawn);
 	if(!hitThisAttack && hitboxActive && hitAI)
 	{
-		if (Cast<ABaseAIController>(hitAI->GetController())->GetState() == TidesStateName::AI_CombatDialogue) {
+		if (Cast<ABaseAIController>(hitAI->GetController())->GetState() == TidesStateName::AI_CombatDialogue || Cast<ABaseAIController>(hitAI->GetController())->GetState() == TidesStateName::AI_RecieveHit) {
 			RequestStateChange(TidesStateName::LockedOnTakeHit);
 		} else {
 			hitThisAttack = true;
