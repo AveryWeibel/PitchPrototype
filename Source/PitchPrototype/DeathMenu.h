2c3e14540c90f408ed1694c4287cbc5899da3672
AveryWeibel <averyweibel@yahoo.com>
2022-05-16 00:36:16 -0700

    Initial cpp files

---
+++ Source/PitchPrototype/DeathMenu.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "DeathMenu.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PITCHPROTOTYPE_API UDeathMenu : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
