1d2a7a725928c4858f2231373c8f9bdffa9625f4
atresonia <atresonia@gmail.com>
2022-05-20 13:30:54 -0700

    Barrel destructs as player runs into it

---
+++ Source/PitchPrototype/Destructible/DestructibleObj.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DestructibleObj.h"
+
+// Sets default values
+ADestructibleObj::ADestructibleObj()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ADestructibleObj::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ADestructibleObj::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
